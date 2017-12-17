1824ee9c6d2cea4c172df566f71ff55f44be7fd9
sunzhijuns <2843359242@qq.com>
2017-12-18 02:17:37 +0800

    使用systembartint

--- app/src/main/java/com/szh/testpic/MainActivity.java
+++ app/src/main/java/com/szh/testpic/MainActivity.java
@@ -1,13 +1,45 @@
 package com.szh.testpic;
 
-import android.support.v7.app.AppCompatActivity;
+import android.annotation.TargetApi;
+import android.app.Activity;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        // 取消显示标题栏
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //判断当前系统版本是否>=Andoird4.4
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            //设置状态栏背景状态
+            //true：表明当前Android系统版本>=4.4
+            setTranslucentStatus(true);
+        }
+
+        //实例化SystemBarTintManager
+        SystemBarTintManager tintManager = new SystemBarTintManager(this);
+        tintManager.setStatusBarTintEnabled(true);
+        // 设置状态栏颜色
+        tintManager.setStatusBarTintResource(R.color.statusbar_bg);
+
+    }
+
+    @TargetApi(19)
+    private void setTranslucentStatus(boolean on) {
+        Window win = getWindow();
+        WindowManager.LayoutParams winParams = win.getAttributes();
+        final int bits = WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS;
+        if (on) {
+            winParams.flags |= bits;
+        } else {
+            winParams.flags &= ~bits;
+        }
+        win.setAttributes(winParams);
     }
 }
