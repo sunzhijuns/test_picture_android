65e02a947351674b1a2a00defd5291347fe8341a
sunzhijuns <2843359242@qq.com>
2017-12-18 05:27:35 +0800

    对话框

--- app/src/main/java/com/szh/testpic/MainActivity.java
+++ app/src/main/java/com/szh/testpic/MainActivity.java
@@ -1,7 +1,13 @@
 package com.szh.testpic;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import com.itheima.materialdialogs.DialogAction;
+import com.itheima.materialdialogs.MaterialDialog;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void showDialog(View view){
+        new MaterialDialog.Builder(this)
+                .title("这是标题")
+                .content("这是描述的内容")
+                .positiveText("确定")
+                .onPositive(new MaterialDialog.SingleButtonCallback() {
+                    @Override
+                    public void onClick(@NonNull MaterialDialog materialDialog, @NonNull DialogAction dialogAction) {
+                        Log.i("点击了确定","----");
+                    }
+                })
+                .negativeText("取消")
+                .onNegative(new MaterialDialog.SingleButtonCallback() {
+                    @Override
+                    public void onClick(@NonNull MaterialDialog materialDialog, @NonNull DialogAction dialogAction) {
+
+                        Log.i("点击了取消","----");
+                    }
+                })
+                .show();
     }
 }
