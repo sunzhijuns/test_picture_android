003a004689bd2bff9327245634d12f572ce1e078
sunzhijuns <2843359242@qq.com>
2017-12-18 08:35:15 +0800

    rxJavaUtil

--- app/src/main/java/com/szh/testpic/MainActivity.java
+++ app/src/main/java/com/szh/testpic/MainActivity.java
@@ -2,12 +2,121 @@ package com.szh.testpic;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import rx.Observable;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.functions.Action1;
+import rx.functions.Func1;
+import rx.schedulers.Schedulers;
+
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private Button mBtn;
+    private String[] cities = new String[]{"beijing", "shenzhen", "shanghai"};
+    private TextView mTv1;
+    private TextView mTv2;
+    private TextView mTv3;
+
+    private static final String LOG_TAG = "测试MainActivity";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mTv1 = findViewById(R.id.tv_szj);
+        mTv2 = findViewById(R.id.tv_szj1);
+        mTv3 = findViewById(R.id.tv_szj2);
+        mTv1.setOnClickListener(this);
+        mTv2.setOnClickListener(this);
+        mTv3.setOnClickListener(this);
+
+        mBtn = findViewById(R.id.btn_all);
+        mBtn.setOnClickListener(this);
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.tv_szj:
+                getCityWeather(cities[0]);
+                break;
+            case R.id.tv_szj1:
+                getCityWeather(cities[1]);
+                break;
+            case R.id.tv_szj2:
+                getCityWeather(cities[2]);
+                break;
+            case R.id.btn_all:
+                getAllWeather();
+                break;
+        }
+
+    }
+
+    private void getAllWeather() {
+        Observable.from(cities).flatMap(new Func1<String, Observable<WeatherData>>() {
+            @Override
+            public Observable<WeatherData> call(String city) {
+                return RxJavaUtil.getWeatherData(city);
+            }
+        }).subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(/*onNext*/new Action1<WeatherData>() {
+                    @Override
+                    public void call(WeatherData weatherData) {
+                        String cityName = weatherData.getName().toLowerCase();
+                        switch (cityName) {
+                            case "beijing":
+                                mTv1.setText(weatherData.toString());
+                                break;
+                            case "shenzhen":
+                                mTv2.setText(weatherData.toString());
+                                break;
+                            case "shanghai":
+                                mTv3.setText(weatherData.toString());
+                                break;
+                        }
+                    }
+                }, /*onError*/new Action1<Throwable>() {
+                    @Override
+                    public void call(Throwable throwable) {
+
+                    }
+                });
+    }
+
+    private void getCityWeather(final String cityName) {
+        RxJavaUtil.getWeatherData(cityName).subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Action1<WeatherData>() {
+                    @Override
+                    public void call(WeatherData weatherData) {
+                        Log.d(LOG_TAG, weatherData.toString());
+                        switch (cityName) {
+                            case "beijing":
+                                mTv1.setText(weatherData.toString());
+                                break;
+                            case "shenzhen":
+                                mTv2.setText(weatherData.toString());
+                                break;
+                            case "shanghai":
+                                mTv3.setText(weatherData.toString());
+                                break;
+                        }
+                    }
+                }, new Action1<Throwable>() {
+                    @Override
+                    public void call(Throwable throwable) {
+                        Log.e(LOG_TAG, throwable.getMessage(), throwable);
+                    }
+                });
     }
 }
---
+++ app/src/main/java/com/szh/testpic/WeatherData.java
@@ -0,0 +1,436 @@
+package com.szh.testpic;
+
+import java.util.List;
+
+/**
+ * Created by sunzhijun on 2017/12/18.
+ */
+
+public class WeatherData {
+
+    /**
+     * coord : {"lon":114.07,"lat":22.55}
+     * weather : [{"id":801,"main":"Clouds","description":"few clouds","icon":"02n"}]
+     * base : stations
+     * main : {"temp":281.85,"pressure":1027,"humidity":74,"temp_min":280.15,"temp_max":283.15}
+     * visibility : 7000
+     * wind : {"speed":5.1,"deg":20}
+     * clouds : {"all":20}
+     * dt : 1513549800
+     * sys : {"type":1,"id":7888,"message":0.0072,"country":"CN","sunrise":1513465050,"sunset":1513503777}
+     * id : 1795565
+     * name : Shenzhen
+     * cod : 200
+     */
+
+    private CoordBean coord;
+    private String base;
+    private MainBean main;
+    private int visibility;
+    private WindBean wind;
+    private CloudsBean clouds;
+    private int dt;
+    private SysBean sys;
+    private int id;
+    private String name;
+    private int cod;
+    private List<WeatherBean> weather;
+
+    @Override
+    public String toString() {
+        return "\n\tWeatherData{coord=" +coord+
+                ",base=" +base+
+                ",main=" +main+
+                ",visibility=" +visibility+
+                ",wind=" +wind+
+                ",dt=" +dt+
+                ",sys=" +sys+
+                ",id=" +id+
+                ",name=" +name+
+                ",cod=" +cod+
+                ",weather=" +weather+
+                "}";
+    }
+
+    public CoordBean getCoord() {
+        return coord;
+    }
+
+    public void setCoord(CoordBean coord) {
+        this.coord = coord;
+    }
+
+    public String getBase() {
+        return base;
+    }
+
+    public void setBase(String base) {
+        this.base = base;
+    }
+
+    public MainBean getMain() {
+        return main;
+    }
+
+    public void setMain(MainBean main) {
+        this.main = main;
+    }
+
+    public int getVisibility() {
+        return visibility;
+    }
+
+    public void setVisibility(int visibility) {
+        this.visibility = visibility;
+    }
+
+    public WindBean getWind() {
+        return wind;
+    }
+
+    public void setWind(WindBean wind) {
+        this.wind = wind;
+    }
+
+    public CloudsBean getClouds() {
+        return clouds;
+    }
+
+    public void setClouds(CloudsBean clouds) {
+        this.clouds = clouds;
+    }
+
+    public int getDt() {
+        return dt;
+    }
+
+    public void setDt(int dt) {
+        this.dt = dt;
+    }
+
+    public SysBean getSys() {
+        return sys;
+    }
+
+    public void setSys(SysBean sys) {
+        this.sys = sys;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getCod() {
+        return cod;
+    }
+
+    public void setCod(int cod) {
+        this.cod = cod;
+    }
+
+    public List<WeatherBean> getWeather() {
+        return weather;
+    }
+
+    public void setWeather(List<WeatherBean> weather) {
+        this.weather = weather;
+    }
+
+    public static class CoordBean {
+        /**
+         * lon : 114.07
+         * lat : 22.55
+         */
+
+        private double lon;
+        private double lat;
+
+        @Override
+        public String toString() {
+            return "\n\tCoordBean{lon = "+lon +
+                    ", lat = "+lat +
+                    "}";
+        }
+
+        public double getLon() {
+            return lon;
+        }
+
+        public void setLon(double lon) {
+            this.lon = lon;
+        }
+
+        public double getLat() {
+            return lat;
+        }
+
+        public void setLat(double lat) {
+            this.lat = lat;
+        }
+    }
+
+    public static class MainBean {
+        /**
+         * temp : 281.85
+         * pressure : 1027
+         * humidity : 74
+         * temp_min : 280.15
+         * temp_max : 283.15
+         */
+
+        private double temp;
+        private int pressure;
+        private int humidity;
+        private double temp_min;
+        private double temp_max;
+
+        @Override
+        public String toString() {
+            return "MainBean{temp = " + temp+
+                    ",pressure = " + pressure+
+                    ",humidity = " +humidity+
+                    ",temp_min= " +temp_min+
+                    ",temp_max= " +temp_max+
+                    "}";
+        }
+
+        public double getTemp() {
+            return temp;
+        }
+
+        public void setTemp(double temp) {
+            this.temp = temp;
+        }
+
+        public int getPressure() {
+            return pressure;
+        }
+
+        public void setPressure(int pressure) {
+            this.pressure = pressure;
+        }
+
+        public int getHumidity() {
+            return humidity;
+        }
+
+        public void setHumidity(int humidity) {
+            this.humidity = humidity;
+        }
+
+        public double getTemp_min() {
+            return temp_min;
+        }
+
+        public void setTemp_min(double temp_min) {
+            this.temp_min = temp_min;
+        }
+
+        public double getTemp_max() {
+            return temp_max;
+        }
+
+        public void setTemp_max(double temp_max) {
+            this.temp_max = temp_max;
+        }
+    }
+
+    public static class WindBean {
+        /**
+         * speed : 5.1
+         * deg : 20
+         */
+
+        private double speed;
+        private int deg;
+
+        @Override
+        public String toString() {
+            return "\n\tWindBean{speed = " +speed+
+                    ",deg= " +deg+
+                    "}";
+        }
+
+        public double getSpeed() {
+            return speed;
+        }
+
+        public void setSpeed(double speed) {
+            this.speed = speed;
+        }
+
+        public int getDeg() {
+            return deg;
+        }
+
+        public void setDeg(int deg) {
+            this.deg = deg;
+        }
+    }
+
+    public static class CloudsBean {
+        /**
+         * all : 20
+         */
+
+        private int all;
+
+        @Override
+        public String toString() {
+            return "\n\tClouds{all= " +all+
+                    "}";
+        }
+
+        public int getAll() {
+            return all;
+        }
+
+        public void setAll(int all) {
+            this.all = all;
+        }
+    }
+
+    public static class SysBean {
+        /**
+         * type : 1
+         * id : 7888
+         * message : 0.0072
+         * country : CN
+         * sunrise : 1513465050
+         * sunset : 1513503777
+         */
+
+        private int type;
+        private int id;
+        private double message;
+        private String country;
+        private int sunrise;
+        private int sunset;
+
+        @Override
+        public String toString() {
+            return "SysBean{type =" +type+
+                    ",id=" +id+
+                    ",message=" +message+
+                    ",country=" +country+
+                    ",sunrise=" +sunrise+
+                    ",sunset=" +sunset+
+                    "}";
+        }
+
+        public int getType() {
+            return type;
+        }
+
+        public void setType(int type) {
+            this.type = type;
+        }
+
+        public int getId() {
+            return id;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+
+        public double getMessage() {
+            return message;
+        }
+
+        public void setMessage(double message) {
+            this.message = message;
+        }
+
+        public String getCountry() {
+            return country;
+        }
+
+        public void setCountry(String country) {
+            this.country = country;
+        }
+
+        public int getSunrise() {
+            return sunrise;
+        }
+
+        public void setSunrise(int sunrise) {
+            this.sunrise = sunrise;
+        }
+
+        public int getSunset() {
+            return sunset;
+        }
+
+        public void setSunset(int sunset) {
+            this.sunset = sunset;
+        }
+    }
+
+    public static class WeatherBean {
+        /**
+         * id : 801
+         * main : Clouds
+         * description : few clouds
+         * icon : 02n
+         */
+
+        private int id;
+        private String main;
+        private String description;
+        private String icon;
+
+        @Override
+        public String toString() {
+            return "WeatherBean{id=" +id+
+                    ",main=" +main+
+                    ",description=" +description+
+                    ",icon=" +icon+
+                    "}";
+        }
+
+        public int getId() {
+            return id;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+
+        public String getMain() {
+            return main;
+        }
+
+        public void setMain(String main) {
+            this.main = main;
+        }
+
+        public String getDescription() {
+            return description;
+        }
+
+        public void setDescription(String description) {
+            this.description = description;
+        }
+
+        public String getIcon() {
+            return icon;
+        }
+
+        public void setIcon(String icon) {
+            this.icon = icon;
+        }
+    }
+}
