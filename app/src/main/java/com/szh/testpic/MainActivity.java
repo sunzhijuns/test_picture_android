df0ebee9ce60b37aaa65f846565c786aff641dfd
sunzhijuns <2843359242@qq.com>
2017-12-19 04:57:00 +0800

    picasso

--- app/src/main/java/com/szh/testpic/MainActivity.java
+++ app/src/main/java/com/szh/testpic/MainActivity.java
@@ -2,12 +2,25 @@ package com.szh.testpic;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
 
 public class MainActivity extends AppCompatActivity {
+    private ImageView iv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        iv = findViewById(R.id.iv);
+
+        Picasso.with(this) //设置context
+                .load("http://d.hiphotos.baidu.com/image/pic/item/ca1349540923dd54bdb23fb8db09b3de9d824819.jpg") //图片url地址
+                .placeholder(R.drawable.ic_launcher_background) //加载时显示的图片
+                .error(R.drawable.ic_launcher_background) //加载错误显示的图片
+                .fit() //自动按照图片尺寸进行压缩
+                .tag("image") //图片tag，便于控制图片加载和暂停加载
+                .into(iv);//图片显示的imageview
     }
 }
