6fd89384579cbdb5fa2387085c37099ae5b974fd
sunzhijuns <2843359242@qq.com>
2017-12-18 23:33:24 +0800

    pinyin

--- app/src/main/java/com/szh/testpic/MainActivity.java
+++ app/src/main/java/com/szh/testpic/MainActivity.java
@@ -3,11 +3,55 @@ package com.szh.testpic;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import net.sourceforge.pinyin4j.PinyinHelper;
+import net.sourceforge.pinyin4j.format.HanyuPinyinCaseType;
+import net.sourceforge.pinyin4j.format.HanyuPinyinOutputFormat;
+import net.sourceforge.pinyin4j.format.HanyuPinyinToneType;
+import net.sourceforge.pinyin4j.format.HanyuPinyinVCharType;
+import net.sourceforge.pinyin4j.format.exception.BadHanyuPinyinOutputFormatCombination;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //-------------------默认格式转换-----------------------------
+        String[] pyStrs = PinyinHelper.toHanyuPinyinStringArray('重');
+
+        for (String s : pyStrs) {
+            System.out.println(s);
+        }
+
+
+
+        //-------------------指定格式转换----------------------------
+        HanyuPinyinOutputFormat format = new HanyuPinyinOutputFormat();
+
+        // UPPERCASE：大写  (ZHONG)
+        // LOWERCASE：小写  (zhong)
+        format.setCaseType(HanyuPinyinCaseType.UPPERCASE);//输出大写
+
+        // WITHOUT_TONE：无音标  (zhong)
+        // WITH_TONE_NUMBER：1-4数字表示音标  (zhong4)
+        // WITH_TONE_MARK：直接用音标符（必须WITH_U_UNICODE否则异常）  (zhòng)
+        format.setToneType(HanyuPinyinToneType.WITH_TONE_MARK);
+
+        // WITH_V：用v表示ü  (nv)
+        // WITH_U_AND_COLON：用"u:"表示ü  (nu:)
+        // WITH_U_UNICODE：直接用ü (nü)
+        format.setVCharType(HanyuPinyinVCharType.WITH_U_UNICODE);
+
+        String[] hao = new String[0];
+        try {
+            hao = PinyinHelper.toHanyuPinyinStringArray('好', format);
+        } catch (BadHanyuPinyinOutputFormatCombination badHanyuPinyinOutputFormatCombination) {
+            badHanyuPinyinOutputFormatCombination.printStackTrace();
+        }
+
+        for (String s : hao) {
+            System.out.println(s);
+        }
     }
 }
